5df4639a4baf857fe4b95c8b78e7ba3773899d30
DavidAbili <89999138+DavidAbili@users.noreply.github.com>
2021-12-07 15:43:17 -0500

    Update Function2.java

--- Function2.java
+++ Function2.java
@@ -1,4 +1,12 @@
-
+/*Class: CMSC203
+ * Instructor: Dr.Alexander
+ * Due 12/4/21
+ * Platform compiler:
+ * I pledge that I have completed the programming assignment independently
+ * I have not copied the code from a student or any source
+ * I have not given my code to any student
+ * David Abili
+ */
 
 
 
